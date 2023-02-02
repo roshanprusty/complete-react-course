901245543a51e77b93432fe16bb0e2121b8afde3
roshan <roshanprusty100@gmail.com>
2023-02-02 11:33:33 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+ReactDOM.render(<h1 className="header">Hello,everyone</h1>, document.getElementById("root"));
+ReactDOM.render(<ul>
+    <li>ROSHAN</li>
+    <li>12017337</li>
+</ul>
+    , document.getElementById("root2"));
